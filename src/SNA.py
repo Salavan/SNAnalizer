f5115804bcfcfece8b12a4bdda639925d55b718b
olzone <osurman@gmail.com>
2016-10-23 21:23:36 +0200

    graphs need to be change to multigraph

--- src/SNA.py
+++ src/SNA.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import copy
 import os
 
 import networkx as nx
@@ -13,22 +14,38 @@ class SNA:
     __book = None
     __characters_influence_per_chapter = None
     __chapters_graphs = []
+    __time_chapters_graphs = []
 
     def __init__(self, book):
         self.__book = book
 
-        self.prepare_data_for_graph(self.__book.get_paragraphs_matrices())
-        # self.build_graph_from_paragraphs()
-        self.build_graph_from_sentences()
 
-        pos = self.get_nodes_pos(nx.compose_all(self.__chapters_graphs))
 
-        # for i in range(len(self.__chapters_graphs)):
-        #     self.draw_chapter_graph(i, "ByParagraphs", pos)
+        case = 0
 
-        for i in range(len(self.__chapters_graphs)):
-            self.draw_chapter_graph(i, "BySentences", pos)
+        if case == 1: #paragraphs
+            self.prepare_data_for_graph(self.__book.get_paragraphs_matrices())
+            self.build_graph_from_paragraphs()
+            pos = self.get_nodes_pos(nx.compose_all(self.__chapters_graphs))
+            for i in range(len(self.__chapters_graphs)):
+                #TODO: use this :p
+                degree_centrality = nx.degree_centrality(self.__chapters_graphs[i])
+                closeness_centrality = nx.closeness_centrality(self.__chapters_graphs[i])
+                eigenvector_centrality = nx.eigenvector_centrality_numpy(self.__chapters_graphs[i])
 
+                self.draw_chapter_graph(i, "ByParagraphs", pos)
+                self.draw_chapter_graph(i, "ByParagraphsWithTime", pos, True)
+            self.draw_chapter_graph("whole", "ByParagraphs", pos)
+
+        else: #sentences
+            self.prepare_data_for_graph(self.__book.get_sentences_matrices())
+            self.build_graph_from_sentences()
+            pos = self.get_nodes_pos(nx.compose_all(self.__chapters_graphs))
+            for i in range(len(self.__chapters_graphs)):
+                self.draw_chapter_graph(i, "BySentences", pos)
+                self.draw_chapter_graph(i, "BySentencesWithTime", pos, True)
+
+            self.draw_chapter_graph("whole", "BySentences", pos)
 
 
     def add_nodes(self, graph):
@@ -63,10 +80,32 @@ class SNA:
         return whole_graph
 
     def build_graphs(self, matrices):
+        rev_character_map = self.__book.get_rev_characters_map()
+        prev_graph = None
+
         for i in range(len(matrices)):
             tmp_graph = nx.Graph()
             self.build_graph_from_chapter(tmp_graph, matrices[i])
             self.__chapters_graphs.append(tmp_graph)
+
+            if prev_graph:
+                for x in range(len(matrices[0])):
+                    for y in range(len(matrices[0])):
+                        try:
+                            prev_graph.edge[rev_character_map[x]][rev_character_map[y]]['weight'] = \
+                                float(prev_graph.edge[rev_character_map[x]][rev_character_map[y]]['weight'])/2
+                        except:
+                            pass
+
+                print(prev_graph.is_multigraph())
+                print(tmp_graph.is_multigraph())
+                self.__time_chapters_graphs.append(nx.compose(prev_graph, tmp_graph)) # albo union...
+
+                prev_graph = self.__time_chapters_graphs[-1]
+            else:
+                prev_graph = copy.deepcopy(tmp_graph)
+
+
 
     def build_graph_from_paragraphs(self):
         self.build_graphs(self.__book.get_paragraphs_matrices())
@@ -74,15 +113,23 @@ class SNA:
     def build_graph_from_sentences(self):
         self.build_graphs(self.__book.get_sentences_matrices())
 
-    def draw_chapter_graph(self, chap, dir, pos = None):
-        graph = self.__chapters_graphs[chap]
-
+    def draw_chapter_graph(self, chap, dir, pos = None, include_time = False):
+        graph = None
+        if chap == "whole" or include_time:
+            graph = nx.compose_all(self.__chapters_graphs)
+        else:
+            graph = self.__chapters_graphs[chap]
         character_map = self.__book.get_characters_map()
 
         nodes_size = np.ones(graph.number_of_nodes())*max_node_size
 
         for n in graph.nodes():
-            nodes_size[graph.nodes().index(n)] *= self.__characters_influence_per_chapter[chap][character_map[n]]
+            if chap == "whole":
+                nodes_size[graph.nodes().index(n)] *= self.__whole_characters_influence[character_map[n]]
+            elif include_time:
+                nodes_size[graph.nodes().index(n)] *= self.__time_inlude_characters_influence_per_chapter[chap][character_map[n]]
+            else:
+                nodes_size[graph.nodes().index(n)] *= self.__characters_influence_per_chapter[chap][character_map[n]]
 
         edges = graph.edges()
         weights = [graph[u][v]['weight'] for u,v in edges]
@@ -101,7 +148,10 @@ class SNA:
         else:
             nx.draw_networkx(graph, node_size=nodes_size, node_color='silver', width=weights, font_size=8)
 
-        plt.savefig(os.path.join("..", "data", "graphs", "{}".format(dir), "{}".format(chap)))
+        if chap == "whole":
+            plt.savefig(os.path.join("..", "data", "graphs", "{}".format(dir), "{}".format("whole")))
+        else:
+            plt.savefig(os.path.join("..", "data", "graphs", "{}".format(dir), "{}".format(chap)))
         plt.clf()
 
 
@@ -110,20 +160,40 @@ class SNA:
         character_map = self.__book.get_characters_map()
 
         characters_influence_per_chapter = {}
+        whole_characters_influence = {}
+        time_inlude_characters_influence_per_chapter = {}
 
+        hist_matrix = np.zeros((len(event_matrices[0]), len(event_matrices[0])))
         for chapter in range(len(event_matrices)):
             matrix = event_matrices[chapter]
+            hist_matrix /= 2
+            hist_matrix += matrix
 
             characters_influence = {}
+            time_characters_influence = {}
             for character in character_map.values():
                 characters_influence[character] = np.sum(matrix[character])
+                time_characters_influence[character] = np.sum(hist_matrix[character])
 
             characters_influence_sum = sum(characters_influence.values())
+            time_characters_influence_sum = sum(time_characters_influence.values())
             for character in character_map.values():
                 characters_influence[character] /= characters_influence_sum
+                time_characters_influence[character] /= time_characters_influence_sum
 
             characters_influence_per_chapter[chapter] = characters_influence
+            time_inlude_characters_influence_per_chapter[chapter] = time_characters_influence
+
+        for character in character_map.values():
+                whole_characters_influence[character] = sum([np.sum(matrix[character]) for matrix in event_matrices])
+
+        whole_characters_influence_sum = sum(whole_characters_influence.values())
+        for character in character_map.values():
+                whole_characters_influence[character] /= whole_characters_influence_sum
+
+        self.__whole_characters_influence = whole_characters_influence
         self.__characters_influence_per_chapter  = characters_influence_per_chapter
+        self.__time_inlude_characters_influence_per_chapter  = time_inlude_characters_influence_per_chapter
 
 
 
